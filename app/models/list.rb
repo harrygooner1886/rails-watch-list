c227fe0e568df5e367085a3b0155d8e6aa0a5211
Harry Salacuri <harry-moto@hotmail.com>
2025-08-15 13:46:45 +0900

    Done

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,8 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  # list.rb
+  has_one_attached :photo
 
   validates :name, uniqueness: true, presence: true
 end
